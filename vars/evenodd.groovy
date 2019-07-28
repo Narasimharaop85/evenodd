52b9d832e113351ed75847af5acf647a95777e8c
Narasimharao <narasimharaop85@gmail.com>
2019-07-28 13:56:45 +0530

    var folder added

---
+++ vars/evenodd.groovy
@@ -0,0 +1,39 @@
+def call(int buildNumber)
+{
+if (buildNumber %2 == 0)
+{
+pipeline{
+agent any
+stages{
+stage('even build number')
+{
+steps{
+echo 'the build number is even'
+script{
+log.info 'starting even build'
+log.warning 'nothing to do'
+}
+}
+}
+}
+}
+}
+else
+{
+pipeline{
+agent any
+stages{
+stage('odd build number')
+{
+steps{
+echo 'the build number is even'
+script{
+log.info 'starting odd build'
+log.warning 'nothing to do'
+}
+}
+}
+}
+}
+}
+}
---
+++ vars/log.groovy
@@ -0,0 +1,6 @@
+def info(message){
+echo "INFO:${message}"
+}
+def info(warning){
+echo "WARNING:${message}"
+}
